09d022f885e47cc84feb6a90b606a7c4b5c05c4b
Fernanda leite <fernandacarolineleite360@gmail.com>
2022-05-04 21:27:48 -0300

    feat: send feedback emails

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import nodemailer from "nodemailer";
 
 import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "669f3991cf5196",
+    pass: "5b805d473c8007"
+  }
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -16,6 +26,18 @@ app.post("/feedbacks", async (req, res) => {
       screenshot
     }
   });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Fernanda Leite <dev.fernandaleite@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
 
   return res.status(201).json({ data: feedback });
 })
